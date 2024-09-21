d7623e578642058005a6639b6226e0677439820d
Kurtik Appadoo <kurtik@Kurtiks-MacBook-Air.local>
2024-09-21 19:34:22 -0400

    initial commit

---
+++ backend_Tourney_Spinner/src/main/java/com/tourney_spinner/backend_tourney_spinner/BackendTourneySpinnerApplication.java
@@ -0,0 +1,13 @@
+package com.tourney_spinner.backend_tourney_spinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTourneySpinnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTourneySpinnerApplication.class, args);
+    }
+
+}
---
+++ backend_Tourney_Spinner/src/test/java/com/tourney_spinner/backend_tourney_spinner/BackendTourneySpinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourney_spinner.backend_tourney_spinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTourneySpinnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
